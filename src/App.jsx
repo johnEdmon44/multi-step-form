577fd54a778521bf92b0c3c1374eaa35c40d63db
Nessaj44 <johnedmoncoc@gmail.com>
2023-11-02 14:25:55 +0800

    Add add-ons step

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import './App.css';
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({name: '', email: '', phoneNumber: ''});
-  const [plan, setPlan] = useState({plan: 'price'});
-  const [addOn, setAddOn] = useState({addOn: 'price'});
+  const [selectedPlan, setSelectedPlan] = useState({value: 'Arcade', monthly: 9});
+  const [addons, setAddons] = useState([    
+    { name: "Online service", description: "Access to multiplayer games", monthly: 1, yearly: 10, checked: false },
+    { name: "Larger storage", description: "Extra 1TB of cloud save", monthly: 2, yearly: 20, checked: false },
+    { name: "Customizable profile", description: "Custom theme on your profile", monthly: 2, yearly: 20, checked: false },
+  ]);
   const [summary, setSummary] = useState({});
 
   const [nameError, setNameError] = useState('');
   const [emailError, setEmailError] = useState('');
-  const [phoneError, setPhoneError] = useState('');
+  const [phoneError, setPhoneError] = useState('');``
 
   const [step1, setStep1] = useState(false);
   const [step2, setStep2] = useState(false);
   const [step3, setStep3] = useState(false);
   const [step4, setStep4] = useState(false);
 
-  const [selectedPlan, setSelectedPlan] = useState({value: 'Arcade', monthly: 9});
   const [isMonthly, setIsMonthly] = useState(true);
   
   const planOption = [
@@ -68,7 +71,9 @@ function App() {
 
 
   const handleGoBack = () => {
-    if (step1) {
+    if (step1 && step2) {
+      setStep2(false);
+    } else if (step1) {
       setStep1(false);
     }
   }
@@ -84,6 +89,21 @@ function App() {
     }
 
     console.log(selectedPlan)
+  }
+
+
+  const handleCheckbox = (e, index) => {
+    const updateAddon = [...addons];
+    updateAddon[index].checked = e.target.checked;
+    setAddons(updateAddon);
+  }
+
+
+  const handleAddAddon = () => {
+    const checkedAddons = addons.filter((addon) => addon.checked === true);
+    setSummary({...summary, checkedAddons});
+    setStep3(true);
+    console.log(summary);
   }
 
 
@@ -108,7 +128,7 @@ function App() {
         </div>
 
         <div className='step'>
-          <p className={`step-indicator ${step2 ? 'active-step': ''}`}>3</p>
+          <p className={`step-indicator ${step1 && step2 && !step3 ? 'active-form': ''}`}>3</p>
           <div className='step-description'>
             <p>step 3</p>
             <p>add-ons</p>
@@ -198,6 +218,32 @@ function App() {
           </div>
         </div>
 
+
+        {/* Pick add-ons */}
+        <div className={`form-section ${step1 && step2 && !step3 ? 'active-form': ''}`}>
+          <h1 className='header'>Pick add-ons</h1>
+          <p className='description'>Add-ons help enhance your gaming experience</p>
+
+          <form>
+            {addons.map((addon, index) => (
+              <label key={index} htmlFor={`checkbox${index}`} className={`checkboxes ${addon.checked ? 'selected-addon': ''}`}>
+                <input type="checkbox" id={`checkbox${index}`} name="checkboxGroup" value={addon.name} onChange={(e) => handleCheckbox(e, index)} />
+                <div className='addon-info'>
+                  <div>
+                    <p className='addon-name'>{addon.name}</p>
+                    <p className='addon-description'>{addon.description}</p>
+                  </div>
+                  <p className='addon-price'>+${isMonthly? `${addon.monthly}/mo`: `${addon.yearly}/yr`}</p>
+                </div>
+              </label>
+            ))}
+          </form>
+
+          <div className='buttons'>
+            <button className='prev-btn' onClick={handleGoBack}>Go back</button>
+            <button className='next-btn' onClick={handleAddAddon}>Next step</button>
+          </div>
+        </div>
 
       </div>
     </section>
